dd25382899e7376054a8f67ed40ad3e6142d8fb8
Sophie Tagar <sophie.tagar@applitools.com>
2021-11-22 11:42:07 -0800

    Update applitools.config.js
    update applitools.config.js to incude more browser environemnts

--- applitools.config.js
+++ applitools.config.js
@@ -8,6 +8,11 @@ module.exports = {
         {width: 1600, height: 1200, name: 'ie11'},
         {width: 1024, height: 768, name: 'edgechromium'},
         {width: 800, height: 600, name: 'safari'},
+        {width: 1000, height: 600, name: 'chrome'},
+        {width: 1000, height: 500, name: 'firefox'},
+        {width: 1000, height: 1200, name: 'ie11'},
+        {width: 1000, height: 768, name: 'edgechromium'},
+        {width: 1000, height: 600, name: 'safari'},
         // Add mobile emulation devices in Portrait mode
         {deviceName: 'iPhone X', screenOrientation: 'portrait'},
         {deviceName: 'Pixel 2', screenOrientation: 'portrait'}
